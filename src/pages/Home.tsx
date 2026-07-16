dd70a8454de0f3388ad3c37ac8aa7e14acabc720
Rubans231 <naranderrubans@gmail.com>
2026-07-16 16:54:12 +0530

    fix: include Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import ParallaxHero from "../components/ParallaxHero";
+import ProjectList from "../components/ProjectList";
+
+export default function Home() {
+  return (
+    <>
+      <ParallaxHero />
+      <ProjectList />
+    </>
+  );
+}

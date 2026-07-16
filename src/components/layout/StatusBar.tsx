8356a9aeb0692a832d474ff19c67e492c4cd3f51
Rubans231 <naranderrubans@gmail.com>
2026-07-16 16:30:55 +0530

    feat: redesign navigation as hyprland-inspired waybar

--- src/components/layout/StatusBar.tsx
+++ src/components/layout/StatusBar.tsx
@@ -1,15 +1,23 @@
 import { NavLink } from "react-router-dom";
+import { useLang } from "../../lib/i18n";
+
+const WORKSPACES = [
+  { n: "1", to: "/", key: "nav.knowledge" },
+  { n: "2", to: "/projects", key: "nav.projects" },
+  { n: "3", to: "/about", key: "nav.about" },
+];
 
 export default function StatusBar() {
+  const { t } = useLang();
+
   return (
     <footer className="statusbar">
-
-      <NavLink to="/">Knowledge</NavLink>
-
-      <NavLink to="/projects">Projects</NavLink>
-
-      <NavLink to="/about">About</NavLink>
-
+      {WORKSPACES.map((w) => (
+        <NavLink key={w.to} to={w.to} end={w.to === "/"} className="workspace-tab">
+          <span className="workspace-n">{w.n}</span>
+          {t(w.key)}
+        </NavLink>
+      ))}
     </footer>
   );
 }
